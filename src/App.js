743c5d7265066f8410c169c5435e8f8d899723d6
kliuchyk <kliuchyk@gmail.com>
2022-07-13 18:25:40 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react';
+import { Dashboard, Preferences, Login } from './components';
+import { useRoutes } from 'react-router-dom';
 
 function App() {
+  const [token, setToken] = useState();
+
+  let element = useRoutes([
+    {
+      path: 'dashboard',
+      element: <Dashboard />,
+    },
+    { path: 'preferences', element: <Preferences /> },
+  ]);
+
+  if (!token) {
+    return <Login setToken={setToken} />;
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <h1>Application</h1>
+      {element}
     </div>
   );
 }
